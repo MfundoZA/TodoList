21907cff3c8521aad3457ee29a7ced40b397b706
Mfundo <jordantenza@gmai.com>
2021-10-31 10:39:56 +0200

    Created methods for manipulating Todo table

---
+++ app/src/main/java/com/mfundoza/todolist/database/TodoDao.java
@@ -0,0 +1,19 @@
+package com.mfundoza.todolist.database;
+
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.mfundoza.todolist.models.Todo;
+
+import java.util.List;
+
+public interface TodoDao {
+    @Insert
+    void insert(Todo todo);
+
+    @Query("SELECT * FROM todo")
+    List<Todo> getAll();
+
+    @Query("DELETE FROM todo WHERE id IN (:todos)")
+    void delete(List<Todo> todos);
+}
