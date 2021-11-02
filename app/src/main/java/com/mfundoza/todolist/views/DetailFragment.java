671377bfbbf6946e7a5dca33808ba2039d36ca41
Mfundo <jordantenza@gmai.com>
2021-11-02 16:44:45 +0200

    Removed unnecessary fields and methods

--- app/src/main/java/com/mfundoza/todolist/views/DetailFragment.java
+++ app/src/main/java/com/mfundoza/todolist/views/DetailFragment.java
@@ -1,12 +1,13 @@
-package com.mfundoza.todolist;
+package com.mfundoza.todolist.views;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.mfundoza.todolist.R;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,44 +16,24 @@ import android.view.ViewGroup;
  */
 public class DetailFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public DetailFragment() {
         // Required empty public constructor
     }
 
     /**
      * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+     * this fragment
      * @return A new instance of fragment DetailFragment.
      */
     // TODO: Rename and change types and number of parameters
     public static DetailFragment newInstance(String param1, String param2) {
         DetailFragment fragment = new DetailFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
