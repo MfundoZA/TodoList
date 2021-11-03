ad8ab78442611c8953ae1316428e0f864660895f
Mfundo <jordantenza@gmai.com>
2021-11-03 10:27:52 +0200

    Removed viewBinding dependencies

--- app/src/main/java/com/mfundoza/todolist/views/MainActivity.java
+++ app/src/main/java/com/mfundoza/todolist/views/MainActivity.java
@@ -1,21 +1,20 @@
 package com.mfundoza.todolist.views;
 
-import androidx.appcompat.app.AppCompatActivity;
-// import androidx.room.Room;
-
 import android.os.Bundle;
 
-import com.mfundoza.todolist.databinding.ActivityMainBinding;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.mfundoza.todolist.R;
+
+
 // import com.mfundoza.todolist.database.*;
 
 public class MainActivity extends AppCompatActivity {
-    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_main);
 
         // AppDatabase db = Room.databaseBuilder(this, AppDatabase.class, "roomDatabase").allowMainThreadQueries().build();
     }
