156b0196c72891d954b07ff19951e24699aef352
Mfundo <jordantenza@gmai.com>
2021-11-07 11:19:20 +0200

    CreateTodoFragment setup

--- app/src/main/java/com/mfundoza/todolist/views/CreateTodoFragment.java
+++ app/src/main/java/com/mfundoza/todolist/views/CreateTodoFragment.java
@@ -1,12 +1,13 @@
-package com.mfundoza.todolist;
+package com.mfundoza.todolist.views;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.mfundoza.todolist.R;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,14 +16,6 @@ import android.view.ViewGroup;
  */
 public class CreateTodoFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public CreateTodoFragment() {
         // Required empty public constructor
@@ -30,29 +23,19 @@ public class CreateTodoFragment extends Fragment {
 
     /**
      * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
+     * this fragment.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment CreateTodoFragment.
      */
     // TODO: Rename and change types and number of parameters
     public static CreateTodoFragment newInstance(String param1, String param2) {
         CreateTodoFragment fragment = new CreateTodoFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
