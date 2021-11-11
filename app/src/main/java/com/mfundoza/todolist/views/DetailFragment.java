f03b49eed479cb58e2f3374512f7494550229486
Mfundo <jordantenza@gmai.com>
2021-11-11 10:03:00 +0200

    Set up databinding in detail fragment

--- app/src/main/java/com/mfundoza/todolist/views/DetailFragment.java
+++ app/src/main/java/com/mfundoza/todolist/views/DetailFragment.java
@@ -5,9 +5,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.mfundoza.todolist.R;
+import com.mfundoza.todolist.databinding.FragmentDetailBinding;
+import com.mfundoza.todolist.viewmodels.MainViewModel;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,6 +21,8 @@ import com.mfundoza.todolist.R;
  * create an instance of this fragment.
  */
 public class DetailFragment extends Fragment {
+    MainViewModel mainViewModel;
+    FragmentDetailBinding binding;
 
     public DetailFragment() {
         // Required empty public constructor
@@ -39,7 +47,18 @@ public class DetailFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false);
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mainViewModel = new ViewModelProvider(requireActivity()).get(MainViewModel.class);
+
+        binding.setViewModel(mainViewModel);
+        binding.setLifecycleOwner(this);
     }
 }
